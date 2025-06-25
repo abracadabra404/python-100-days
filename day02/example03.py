f0df196184a123aadcabd6baf26e4666f1092388
xvyaxi <xvyaxi@outlook.com>
2025-06-25 14:54:29 +0800

    add example02-03

---
+++ day02/example03.py
@@ -0,0 +1,32 @@
+# 二进制
+print(0b100)
+
+# 八进制
+print(0o100)
+# 十进制
+print(100)
+
+# 十六进制
+print(0x100)
+
+# 数学写法
+print(123.456)
+
+# 科学计数法
+print(1.23456e2)
+
+# 使用type函数检查变量的类型
+a = 100;
+b = 123.44
+c = 'hello,python'
+d = True;
+e = '123.45'
+f = '123'
+g = '100'
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# 变量的类型转换操纵
