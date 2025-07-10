5970fef99f948a044a4198713bef1a1a46b06f26
xvyaxi <xvyaxi@outlook.com>
2025-07-10 11:12:34 +0800

    add example05

---
+++ day01-20/day05/example05.py
@@ -0,0 +1,35 @@
+"""
+BMI计算器
+"""
+
+height = float(input('Your height(cm):'))
+weight = float(input('Your weight(kg):'))
+bmi = weight / (height / 100) ** 2
+print(f'{bmi = : .1f}')
+
+if bmi < 18.5:
+    print('Your body is underweight')
+elif bmi < 24:
+    print('Your body is great')
+elif bmi < 27:
+    print('Your body is overweight')
+elif bmi < 30:
+    print('Your body is mild obesity')
+elif bmi < 35:
+    print('Your body is moderate obesity')
+else:
+    print('Your body is server obesity')
+
+"""
+match-case 结构
+"""
+status_code = int(input('response code:'))
+
+match status_code:
+    case 400 | 405 : description = 'Invalid Request'
+    case 401 | 403 : description = 'Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    case _: description = 'Unknown Status Code'
+print('status code description:',description)
+
